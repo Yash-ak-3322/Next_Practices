64273c6eba342709d6504f02d3d0bb9c5f41095a
Yash-ak-3322 <yaakbari1@myseneca.ca>
2024-10-27 11:41:53 -0400

    Started with Next JS

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <>
+      <header className="flex justify-center text-blue-900 text-2xl font-thin p-5">
+        About Page :)
+      </header>
+    </>
+  );
+}
+
+export default About;
---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+function Profile() {
+  return (
+    <>
+      <header className="flex justify-center text-blue-900 text-2xl font-thin p-5">
+        Profile Page :)
+      </header>
+    </>
+  );
+}
+
+export default Profile;
